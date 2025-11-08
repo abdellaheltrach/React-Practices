740a9086f79c22d4e21cf3fd6735763ecc99556b
abdellaheltrach <abdellah.eltrach@gmail.com>
2025-11-08 17:42:50 +0100

    adding List Rendering to the page

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,26 @@ import { Post } from "./Post";
 import { Side } from "./Side";
 
 function App() {
+  const PostArr: { id?: number; Title?: string; Body?: string }[] = [
+    { id: 1, Title: "Hello there", Body: "hi from the body :-)" },
+    { id: 2, Title: "how is the day today", Body: "hi from the body :-)" },
+    { id: 3, Title: "news", Body: "hi from the body :-)" },
+    { id: 4, Title: "React is great", Body: "hi from the body :-)" },
+  ];
+
+  const PostList = PostArr.map((p, indedx) => (
+    <div key={p.id ?? indedx}>
+      <Post Title={p.Title} Body={p.Body} />
+    </div>
+  ));
+
   return (
     <div>
       <Header />
       <div className="flex justify-center ">
         <div className="flex justify-center gap-5 w-[70%]">
           <div className="w-[50%]">
-            <Post Title="Hello there" Body="hi from the body :-)" />
-            <Post Title="how is the day today" Body="hi from the body :-)" />
-            <Post Title="news" Body="hi from the body :-)" />
-            <Post Title="React is great" Body="hi from the body :-)" />
-            <Post />
+            {PostList}
           </div>
           <div className="w-[30%]">
             <Side />
--- src/Side.tsx
+++ src/Side.tsx
@@ -12,28 +12,59 @@ function Button({ children }: { children?: React.ReactNode }) {
     </div>
   );
 }
+
+const buttonArr: {
+  key?: number;
+  Children?: React.ReactNode;
+  Title?: string;
+}[] = [
+  {
+    key: 1,
+    Title: "Woow",
+    Children: (
+      <div>
+        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://cdn.naturettl.com/wp-content/uploads/2023/06/29133512/macro-flower-photography-8-800x593.jpg"
+            alt="flower"
+            width={100}
+            height={75}
+          />
+        </a>
+      </div>
+    ),
+  },
+  {
+    key: 2,
+    Title: "Amazing",
+    Children: (
+      <div>
+        <a href="https://example.org" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
+            alt="cat"
+            width={100}
+            height={75}
+          />
+        </a>
+      </div>
+    ),
+  },
+];
+
+const ButtonList = buttonArr.map((btn, index) => {
+  return (
+    <Button key={btn.key ?? index}>
+      <h3>{btn.Title}</h3>
+      {btn.Children}
+    </Button>
+  );
+});
+
 export function Side() {
   return (
     <div className="border-4 my-4 py-6 border-cyan-600 p-4">
-      <div className="flex flex-wrap gap-3">
-        {/* Button 1 */}
-        <Button>
-          <h3>Woow</h3>
-          <a
-            href="https://example.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://cdn.naturettl.com/wp-content/uploads/2023/06/29133512/macro-flower-photography-8-800x593.jpg"
-              alt="flower"
-              width={100}
-              height={75}
-            />
-          </a>
-        </Button>
-
-      </div>
+      <div className="flex flex-wrap gap-3">{ButtonList}</div>
     </div>
   );
 }
