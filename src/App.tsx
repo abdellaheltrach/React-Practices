9926dbcaa5a473cae320e3acba347c68dc6889b8
abdellaheltrach <abdellah.eltrach@gmail.com>
2025-11-09 18:50:54 +0100

    Update array challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,54 @@
 import { useState } from "react";
 
-interface Player {
-  firstName: string;
-  lastName: string;
-  score: number;
+interface Iproduct {
+  id: number;
+  name: string;
+  count: number;
 }
 
-export default function Scoreboard() {
-  const [player, setPlayer] = useState<Player>({
-    firstName: "Ranjani",
-    lastName: "Shettar",
-    score: 10,
-  });
+const initialProducts: Iproduct[] = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 5,
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+  },
+];
 
-  function handlePlusClick() {
-    setPlayer({ ...player, score: player.score + 1 });
-  }
+export default function ShoppingCart() {
+  const [products, setProducts] = useState<Iproduct[]>(initialProducts);
 
-  function handleFirstNameChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setPlayer({
-      ...player,
-      firstName: e.target.value,
-    });
-  }
-
-  function handleLastNameChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setPlayer({
-      ...player,
-      lastName: e.target.value,
-    } ); 
+  function handleIncreaseClick(productId: number) {
+    setProducts(
+      products.map((p) => {
+        return p.id === productId ? { ...p, count: p.count + 1 } : p;
+      })
+    );
   }
 
   return (
-    <div className="p-6 max-w-md mx-auto bg-gray-100 rounded-lg shadow-md space-y-4">
-      <label className="block text-lg">
-        Score: <b>{player.score}</b>{" "}
-        <button
-          className="ml-2 px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
-          onClick={handlePlusClick}
-        >
-          +1
-        </button>
-      </label>
-
-      <label className="block">
-        First name:
-        <input
-          className="ml-2 border border-gray-300 rounded px-2 py-1"
-          value={player.firstName}
-          onChange={handleFirstNameChange}
-        />
-      </label>
-
-      <label className="block">
-        Last name:
-        <input
-          className="ml-2 border border-gray-300 rounded px-2 py-1"
-          value={player.lastName}
-          onChange={handleLastNameChange}
-        />
-      </label>
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
